2b132bf46674836b03b18c5118798b02e73baaa0
albator105 <77986643+albator105@users.noreply.github.com>
2022-07-24 19:27:43 +0200

    ajout musi desormais il est possible de mettre la musique du jeu

--- main.py
+++ main.py
@@ -13,24 +13,23 @@ def set_difficulty(value, difficulty):
 def start_the_game():
     # lancement du jeu
     pass
-
-
+image_menu = pygame_menu.baseimage.BaseImage(
+    image_path=("image/fond_menu.jpg")
+)
+menu_theme = pygame_menu.Theme(background_color=image_menu, # transparent background
+                title_background_color=(225, 98, 117),
+                title_font_shadow=True,
+                widget_padding=25)
 # creation de la fenetre et de son titre
 pygame.display.set_caption("JEUX MINOU")
-screen = pygame.display.set_mode((1280, 700))
-image_menu = pygame.image.load("image/fond_menu.jpg")  # importation de l'image
-image_menu = pygame.transform.scale(image_menu, (1280, 700))
-pygame.display.update()  # mise a jour de l'écran (refresh)
-menu = pygame_menu.Menu("Bonjour", 1280, 700, theme=pygame_menu.themes.THEME_BLUE)
-menu.add.text_input("Name :", default="Quentin")
-menu.add.selector("Difficulty :", [("Hard", 1), ("Easy", 2)], onchange=set_difficulty)
+pygame.mixer.music.load("son/music1.mp3")
+screen = pygame.display.set_mode((1280, 700),pygame.FULLSCREEN)
+menu = pygame_menu.Menu("Dragon Breath", 1280, 700,theme=menu_theme)
+menu.add.text_input("Name :", default="")
+menu.add.selector("Difficulty :", [("Facile", 1), ("Moyen", 2)], onchange=set_difficulty)
 menu.add.button("Play", start_the_game)
 menu.add.button("Quit", pygame_menu.events.EXIT)
+menu.add.button("music stop", pygame.mixer.music.stop)
+menu.add.button("music start", pygame.mixer.music.play)
+pygame.display.update()  # mise a jour de l'écran (refresh)
 menu.mainloop(screen)
-
-# run = True
-# while run:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             run = False
-#             pygame.exit
