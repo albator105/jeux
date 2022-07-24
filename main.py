900a77cd5b19640adfbe6ef9edbfd080d4defd19
albator105 <77986643+albator105@users.noreply.github.com>
2022-07-24 15:23:08 +0200

    image redimensionné

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+#importation des librairie
 import pygame
-pygame.init()
-screen = pygame.display.set_mode((1080, 500))
+pygame.init() #initialisation de pygame
+
+
+#creation de la fenetre et de son titre
+screen = pygame.display.set_mode((1280, 700))
 pygame.display.set_caption('JEUX MINOU')
-pygame.display.update()
+
+image_menu = pygame.image.load("image/fond_menu.jpg") # importation de l'image
+image_menu = pygame.transform.scale(image_menu, (1280,700))
+screen.blit(image_menu, (0,0))
+pygame.display.update() # mise a jour de l'écran (refresh)
 run = True
-while run == True:
+while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-exit
+            pygame.exit
+
+
         
